03eb56527348f1b1735d0394229735b344157deb
Felipe Cody <fcody@et-a2.kzn.21-school.ru>
2021-10-23 23:49:39 +0300

    pipes fix

--- main.c
+++ main.c
@@ -12,7 +12,7 @@
 
 void close_fd(t_elem *elem)
 {
-	if(elem->prev)
+	if(elem->prev && elem->prev->pfd[0] > 2)
 		close(elem->prev->pfd[0]);
 	if(elem->pfd[1] != -1)
 		close(elem->pfd[1]);
@@ -45,7 +45,7 @@ void execution(t_elem *elem)
     {
         if(elem->data->debug)
         	dprintf(2,"\n>>> %.05d: now %p\n", elem->pid, elem);
-        builtin_check(elem);
+        //builtin_check(elem);
         if(!elem->is_builtin)
             find_path(elem);
         if(elem->data->debug)
@@ -104,14 +104,11 @@ void execution(t_elem *elem)
                 if (elem->data->debug)
                 	dprintf(2, ">>> %.05d: child\n", elem->pid);
                 if (!elem->is_builtin && (execve(elem->cmd[0], elem->cmd, elem->data->envp) < 0))
-                    ft_putstr_fd(strerror(errno), 2);
+                {
+					ft_putstr_fd(strerror(errno), 2);
+					exit(1);
+				}
             }
-            else
-            {
-				wait(0);
-				if (elem->data->debug)
-					dprintf(2, ">>> %.05d: join\n", elem->pid);
-			}
         }
         else
         {
@@ -129,16 +126,7 @@ void execution(t_elem *elem)
             elem = elem->next;
         }
         else
-        {
-//            if(elem->pfd[1] != -1)
-//            {
-//                if(elem->data->debug)
-//                dprintf(2,">>> closing current pipe\n");
-//                close(elem->pfd[1]);
-//                close(elem->pfd[0]);
-//            }
             break;
-        }
     }
 }
 
@@ -153,6 +141,19 @@ void init(t_data *data, char **env)
     data->simple_redirect_output_fd = -1;
     data->double_redirect_output_fd = -1;
     env_path_find(data);
+}
+
+void waiting(t_data *data)
+{
+	t_elem *elem = data->elem_start;
+	while (elem)
+	{
+		wait(0);
+		if(elem->next)
+			elem = elem->next;
+		else
+			break;
+	}
 }
 
 int main(int ac, char **av, char **env)
@@ -171,32 +172,32 @@ int main(int ac, char **av, char **env)
         data->debug = 1;
     data->elem_start = push_back(data->elem_start, data);
     data->elem_start = push_back(data->elem_start, data);
-//    data->elem_start->next = push_back(data->elem_start->next, data);
+    data->elem_start->next = push_back(data->elem_start->next, data);
 //    data->elem_start->next->next = push_back(data->elem_start->next->next, data);
 //    data->elem_start->next->next->next = push_back(data->elem_start->next->next->next, data);
 //    data->elem_start->next->next->next->next = push_back(data->elem_start->next->next->next->next, data);
 //    data->elem_start->next->next->next->next->next = push_back(data->elem_start->next->next->next->next->next, data);
 
     data->elem_start->cmd = cmd;
-    data->elem_start->cmd[0] = "cat";
-    data->elem_start->cmd[1] = 0;
+    data->elem_start->cmd[0] = "echo";
+    data->elem_start->cmd[1] = "test";
     data->elem_start->cmd[2] = 0;
     data->elem_start->cmd[3] = 0;
     data->elem_start->type = PIPE;
 
     data->elem_start->next->cmd = cmd2;
-    data->elem_start->next->cmd[0] = "ls";
-    data->elem_start->next->cmd[1] = 0;
+    data->elem_start->next->cmd[0] = "cat";
+    data->elem_start->next->cmd[1] = "-e";
     data->elem_start->next->cmd[2] = 0;
     data->elem_start->next->cmd[3] = 0;
-    data->elem_start->next->type = CMD;
-//
-//    data->elem_start->next->next->cmd = cmd3;
-//    data->elem_start->next->next->cmd[0] = "ls";
-//    data->elem_start->next->next->cmd[1] = 0;
-//    data->elem_start->next->next->cmd[2] = 0;
-//    data->elem_start->next->next->type = CMD;
-//
+    data->elem_start->next->type = PIPE;
+
+    data->elem_start->next->next->cmd = cmd3;
+    data->elem_start->next->next->cmd[0] = "cat";
+    data->elem_start->next->next->cmd[1] = "-e";
+    data->elem_start->next->next->cmd[2] = 0;
+    data->elem_start->next->next->type = CMD;
+
 //    data->elem_start->next->next->next->cmd = cmd4;
 //    data->elem_start->next->next->next->cmd[0] = "./text/old.txt";
 //	data->elem_start->next->next->next->cmd[1] = 0;
@@ -221,6 +222,7 @@ int main(int ac, char **av, char **env)
 	simple_redirects(data);
 //	printf(">>> DO %d SO %d SI %d\n", data->double_redirect_output_fd,data->simple_redirect_output_fd,data->simple_redirect_input_fd);
     execution(data->elem_start);
+    waiting(data);
 //    print_elems(data->elem_start);
     return 0;
 }
